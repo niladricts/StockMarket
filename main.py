d21882c69328a307832e211a23145c18e09ef538
Niladri Saha <bsensous@gmail.com>
2021-04-28 14:18:25 +0300

    Update main.py
    Refactored for better understanding

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import yfinance as yf
 
 PandasDataFrame = TypeVar("pandas.core.frame.DataFrame")
 
-
+# class for stock market analysis
 class StockMarket:
     startDate = ''
     endDate = ''
@@ -29,7 +29,10 @@ class StockMarket:
         return dt.strftime("%Y-%m-%d")
 
     @staticmethod
-    def get_upward_trend(df: PandasDataFrame, start_dt: datetime, end_dt: datetime) -> NoReturn:
+    def show_upward_trend(df: PandasDataFrame, start_dt: datetime, end_dt: datetime) -> NoReturn:
+        """
+        This method is used to show consecutive upward for a certain date range for a particular stock
+        """
         trend_days = 0
         df_sorted = df.sort_index()
         close_list = [row for row in df_sorted["Close"]]
@@ -42,13 +45,14 @@ class StockMarket:
         print(f"Total consecutive upward trend from {start_dt} to {end_dt} : {trend_days} days")
 
     @staticmethod
-    def get_sma_5_days_opening(dft: PandasDataFrame, name: str) -> NoReturn:
+    def show_sma_5_days_opening(dft: PandasDataFrame, name: str) -> NoReturn:
         """
         This method is used to get the 5 days Simple Moving Coverage of the closing price and
         compare it with the opening price of the date where opening price is higher than the said
         SMA
         """
-        price_percentage = pd.Series([])
+        # to store the price percentage change in another data frame
+        price_percentage = pd.Series([]) 
         dft.set_index('Date')
         dft['5 Days SMA Closing'] = dft.Close.rolling(window=5).mean()
         new_dft = dft[['Date', 'Open', '5 Days SMA Closing']]
@@ -61,7 +65,7 @@ class StockMarket:
                 trading_date = new_dft["Date"][i]
                 opening_amt = new_dft["Open"][i]
                 sma_closing_amt = new_dft["5 Days SMA Closing"][i]
-
+        # new data frame created with columns 'Date','Open','5 Days SMA Closing', 'price_change_percentage"
         new_dft.insert(3, "price_change_percentage", price_percentage)
         new_dft.sort_values(by=['price_change_percentage'], inplace=True)
         print(new_dft)
@@ -75,7 +79,9 @@ class StockMarket:
         date02 = self.date_format(dt2)
 
         try:
+            # download the data in data frame format directly using yfinance module based on date range
             data = yf.download(stkname, start=date01, end=date02)
+            # Reset the index of the dataframe
             data.reset_index(inplace=True, drop=False)
             print(f"Actual Dataset \n {data}")
             # data.plot()
@@ -85,20 +91,20 @@ class StockMarket:
 
         if opt == 1:
             print("********* Consecutive Upward Trend *********")
-            self.get_upward_trend(data, date01, date02)
+            self.show_upward_trend(data, date01, date02)
         # elif opt == 2:
         # self.get_highest_trading_with_stock(data, date01, date02)
         elif opt == 3:
-            self.get_sma_5_days_opening(data, stkname)
+            self.show_sma_5_days_opening(data, stkname)
         else:
             print("Bad Option")
             exit(0)
-        exit(0)
+     
 
 
 if __name__ == '__main__':
-    start_date = input('Enter Start Date')
-    end_date = input('Enter End Date')
+    start_date = input('Enter Start Date. Date should be like this YYYY,MM,DD:')
+    end_date = input('Enter End Date. Date should be like this YYYY,MM,DD:')
     stock_name = input("Enter Stock Name")
     option = int(input("Enter your option \n Press 1 for Consecutive Upward Trend"
                        "\n Press 2 for Highest Trading Volume and Significant Stock Price"
